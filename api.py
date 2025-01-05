3d0fc9ef281344fc54dc442a8e1e9f22fa8f1adf
Elephant200 <alexktev@gmail.com>
2025-01-05 18:14:57 -0500

    Add error handling

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from routers import vectors, matrices, primes, geometry, triangle_solver, irrationals, cas
 
 app = FastAPI(title="The Elephant Calculator API")
@@ -10,6 +11,13 @@ app.include_router(geometry.router, prefix="/geometry", tags=["Geometry"])
 app.include_router(triangle_solver.router, prefix="/triangles", tags=["Triangle Solver"])
 app.include_router(irrationals.router, prefix="/irrationals", tags=["High-Precision"])
 app.include_router(cas.router, prefix="/cas", tags=["CAS"])
+
+@app.exception_handler(Exception)
+async def value_error_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=400,
+        content={"detail": f"Error: {exc}"}
+    )
 
 # For testing purposes
 if __name__ == "__main__":
