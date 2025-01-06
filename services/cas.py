b65478690b5b3e5235098e399989448e0084360a
Elephant200 <alexktev@gmail.com>
2025-01-06 13:16:12 -0500

    Refactored cas formatters

--- services/cas.py
+++ services/cas.py
@@ -1,34 +1,13 @@
 from sympy import *
 import os
 import platform
+from utils.formatters import format_expression, reverse_format
+
 w, x, y, z, a, b, c, d = symbols("w x y z a b c d")
 
 clear_cmd = "cls" if platform.system() == "Windows" else "clear"
 def clear():
     os.system(clear_cmd)
-
-def format_expression(expression, diff=False):
-    if diff:
-        expression = expression.replace("'", ".diff()")
-    for i in list("wxyzabcd)"):
-        for j in ['w','x','y','z','a','b','c','d','(','f','g','h','exp','ln','sin','cos','tan']:
-            expression = expression.replace(f'{i}{j}', f'{i}*{j}')
-            for k in range(10):
-                expression = expression.replace(f'{i}^{k}{j}', f'({i}^{k})*{j}')
-    expression = expression.replace('^', '**').replace(')(', ')*(').replace('infinity', 'oo').replace('infty', 'oo').replace(f'e^{x}', f'exp({x})')
-    for i in list("wxyzabcd(fe"):
-        for num in range(10):
-            expression = expression.replace(f'{num}{i}', f'{num}*{i}')
-    return expression
-
-def reverse_format(expression):
-    expression = expression.replace('**', '^').replace(')*(', ')(').replace('x*(', 'x(')
-    for i in range(10):
-        expression = expression.replace(f'{i}*', f'{i}')
-    for i in list("wxyzabcd)"):
-        for j in ['w','x','y','z','a','b','c','d','(','f','g','h','exp','ln','sin','cos','tan']:
-            expression = expression.replace(f'{i}*{j}', f'{i}{j}')
-    return expression
 
 def factor_expression(expression):
     try:
