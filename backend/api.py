241accf67d855f98540e87208e60760b2c39ed7b
Elephant200 <alexktev@gmail.com>
2025-01-08 20:34:56 -0500

    Switch localhost api to port 0.0.0.0

--- backend/api.py
+++ backend/api.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "api:app",
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=8000,
         reload=True
     )
