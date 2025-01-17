df190b7c57ed7935250d124719b561887ca7db31
Elephant200 <alexktev@gmail.com>
2025-01-17 11:15:53 -0500

    Add triangle to main page

--- frontend/elephant-calculator/app/page.tsx
+++ frontend/elephant-calculator/app/page.tsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
+      <div
+        className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+      />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         
